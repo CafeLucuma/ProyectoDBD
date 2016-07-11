ef6498278fe3b871a6f6cf751eab80969430d4c7
CafeLucuma <oscar.carrasco@usach.cl>
2016-07-11 19:25:12 -0400

    se puede buscar por especialidad y medico. Se puede reservar hora (faltan mensajes popup)

---
+++ app/models/reserved_hour.rb
@@ -0,0 +1,12 @@
+class ReservedHour < ActiveRecord::Base
+
+	def self.crear(paciente_id, ab_id, hora_inicio, hora_fin)
+		
+		hora_inicio2 = Time.parse(hora_inicio).to_s(:db)
+		hora_fin2 = Time.parse(hora_fin).to_s(:db)
+		created = Time.zone.now.to_s(:db)
+		updated = Time.zone.now.to_s(:db)
+		sql = "INSERT INTO reserved_hours (patient_id, AB_ID, RH_START_TIME, RH_FINISH_TIME, RH_STATE, RH_ASSISTANCE, RH_DERIVED, created_at, updated_at) VALUES ('#{paciente_id}', '#{ab_id}', '#{hora_inicio2}', '#{hora_fin2}', true, false, false, '#{created}','#{updated}')"
+		ActiveRecord::Base.connection.execute(sql)
+	end
+end
---
+++ test/models/reserved_hour_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ReservedHourTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
