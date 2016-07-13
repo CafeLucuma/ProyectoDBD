e016b415492e35057ee21c6b747696eb44f592af
CafeLucuma <oscar.carrasco@usach.cl>
2016-07-12 22:37:10 -0400

    mishoras más decentes

--- app/controllers/my_hours_controller.rb
+++ app/controllers/my_hours_controller.rb
@@ -4,8 +4,12 @@ before_action :authenticate_user!
   def mishoras
 	@horas = ReservedHour.find_by_sql("select * from reserved_hours rh inner join patients p on p.patient_id = rh.patient_id inner join users u on p.user_id = u.user_id where u.user_id = '#{current_user.user_id}' and rh.RH_STATE = true")
 
-	@abs = AttentionBlock.find_by_sql("select * from attention_blocks")
-	
+	@abs = AttentionBlock.find_by_sql("select * from attention_blocks")	
+	@doctors = Doctor.find_by_sql("select * from doctors")
+	@ests = Establishment.find_by_sql("select * from establishments")
+	@comunas = Commune.find_by_sql("select * from communes")
+	@specs = Specialty.find_by_sql("select * from specialties")
+
   end
 
   def mishoras2
@@ -19,7 +23,7 @@ before_action :authenticate_user!
 	#poner mensaje de que su hora ha sido eliminada
 	redirect_to welcome_index_path
 
-  end 
+  end
 
   def message1
   	#cancelar horas y enviar a vista message1
